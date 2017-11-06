dfbe47d5c19f123fea83003fecc94a1fcf9629eb
Szymon Halastra <szymon.halastra@gmail.com>
2017-11-06 14:50:42 +0100

    Initialize repository

---
+++ src/main/java/io/shalastra/facerecognition/FacerecognitionApplication.java
@@ -0,0 +1,12 @@
+package io.shalastra.facerecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacerecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacerecognitionApplication.class, args);
+	}
+}
