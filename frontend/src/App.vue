d90a2e55141cc685cdecf6d66b5e0cc29d5d7c8a
Szymon Halastra <szymon.halastra@gmail.com>
2018-01-04 15:15:43 +0100

    Add webcam component to frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,50 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <b-container fluid>
+      <b-row align-h="center">
+        <b-col cols="5">
+          <webcam ref="webcam"></webcam>
+        </b-col>
+        <b-col cols="5">
+          <img :src="this.img" style="margin-top:60px;width:500px;height:380px;"/>
+        </b-col>
+      </b-row>
+      <b-row align-h="center">
+        <b-col cols="2">
+          <b-button type="button" @click="photo">Capture Photo</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import Webcam from 'vue-web-cam/src/webcam'
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        img: null
+      }
+    },
+    methods: {
+      photo() {
+        this.img = this.$refs.webcam.capture()
+      }
+    },
+    components: {
+      Webcam
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
